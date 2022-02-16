7fc253c8bf34933cc86df96d6bbfb0f95eccb1a9
David Meier <davidmeier1@gmail.com>
2022-02-16 21:39:34 +0100

    simplify cors config

--- src/main/java/com/github/davemeier82/homeautomation/demo/HomeAutomationApplication.java
+++ src/main/java/com/github/davemeier82/homeautomation/demo/HomeAutomationApplication.java
@@ -30,11 +30,7 @@ import com.github.davemeier82.homeautomation.zigbee2mqtt.Zigbee2MqttDeviceFactor
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -90,24 +86,4 @@ public class HomeAutomationApplication {
     return new HiveMqMqttClient(eventFactory, eventPublisher, serverHost, serverPort, username, password);
   }
 
-  @Bean
-  public FilterRegistrationBean<CorsFilter> corsFilter(
-      @Value("${allowedOrigins:null}") List<String> allowedOrigins,
-      @Value("${allowedHeaders:null}") List<String> allowedHeaders,
-      @Value("${allowedMethods:GET,HEAD,POST}") List<String> allowedMethods,
-      @Value("${exposedHeaders:null}") List<String> exposedHeaders
-  ) {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.setAllowedOrigins(allowedOrigins);
-    config.setAllowedHeaders(allowedHeaders);
-    config.setAllowedMethods(allowedMethods);
-    config.setExposedHeaders(exposedHeaders);
-
-    source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    bean.setOrder(0);
-    return bean;
-  }
 }
---
+++ src/main/java/com/github/davemeier82/homeautomation/demo/WebMvcConfig.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.davemeier82.homeautomation.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  private final String[] allowedOrigins;
+  private final String[] allowedHeaders;
+  private final String[] allowedMethods;
+  private final String[] exposedHeaders;
+
+  public WebMvcConfig(
+      @Value("${allowedOrigins:null}") String[] allowedOrigins,
+      @Value("${allowedHeaders:*}") String[] allowedHeaders,
+      @Value("${allowedMethods:GET,HEAD,POST,PUT}") String[] allowedMethods,
+      @Value("${exposedHeaders:null}") String[] exposedHeaders
+  ) {
+    this.allowedOrigins = allowedOrigins;
+    this.allowedHeaders = allowedHeaders;
+    this.allowedMethods = allowedMethods;
+    this.exposedHeaders = exposedHeaders;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods(allowedMethods)
+        .allowedHeaders(allowedHeaders)
+        .exposedHeaders(exposedHeaders);
+  }
+}
